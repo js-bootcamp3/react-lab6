3fed9669a850227c1a970435560ecb2793c8c2d7
Mansur Mabo <mansurmabo@gmail.com>
2022-08-26 11:57:17 +0600

    add side menu

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import {useState} from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Home from './containers/Home/Home';
+import {Layout} from 'antd'
 
+import Home from './containers/Home/Home';
+import MenuComponent from './components/Menu/MenuComponent';
+
+import 'antd/dist/antd.css';
 import './App.css';
+import Products from './containers/Products/Products';
+
+const {Sider} = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+  
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider 
+        collapsible 
+        collapsed={collapsed} 
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <MenuComponent />
+      </Sider>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Layout>
   );
 }
 
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Button } from 'antd'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <Button type="primary">Button</Button>  
+      <Button type="link">Button</Button>  
+    </div>
+    
   )
 }
 
